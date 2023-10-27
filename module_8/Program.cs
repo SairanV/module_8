3ab4859df5ce963ecff851984038e60f6d6ed0e1
aidyn <aidynmaskutov2004@gmail.com>
2023-10-27 23:06:57 +0600

    Добавьте файлы проекта.

---
+++ module_8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 5; 
+            SquarIndex indexer = new SquarIndex(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                indexer[i] = i;
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Element {i}: {indexer[i]}");
+            }
+        }
+    }
+}
